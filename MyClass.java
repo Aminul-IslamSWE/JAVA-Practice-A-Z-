38f22b3a8f93f225d79dda4b3c658198be57ccbb
Md Aminul Islam Raju <86077957+Raz2396@users.noreply.github.com>
2024-01-21 00:05:41 +0600

    Add files via upload

--- MyClass.java
+++ MyClass.java
@@ -1,25 +1,36 @@
-public class MyClass {
+package Methods;
 
-    private String name;
-    private int age;
+public class MyClass {
+    // Instance variable
+    private int value;
 
     // Constructor
-    public MyClass(String name, int age) {
-        this.name = name;
-        this.age = age;
+    public MyClass(int value) {
+        this.value = value;
     }
 
-    // Method
-    public void displayInfo() {
-        System.out.println("Name: " + name + ", Age: " + age);
+    // Instance method
+    public void displayValue() {
+        System.out.println("The current value is " + this.value);
     }
 
-    // Main method (entry point of the program)
+    // Another instance method that modifies the instance variable
+    public void setValue(int newValue) {
+        this.value = newValue;
+    }
+
     public static void main(String[] args) {
         // Creating an instance of the class
-        MyClass myInstance = new MyClass("aminul Islam Raju", 24);
+        MyClass obj = new MyClass(42);
 
-        // Calling the method on the instance
-        myInstance.displayInfo();
+        // Calling instance methods
+        obj.displayValue();  // Output: The current value is 42
+
+        // Modifying the instance variable
+        obj.setValue(100);
+
+        // Calling the method again to see the updated value
+        obj.displayValue();  // Output: The current value is 100
     }
 }
+
